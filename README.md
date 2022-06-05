ad98efc57644bf303f72a12c23c367a28eba26b9
Abby Rosenthal <adlundeen@gmail.com>
2022-06-04 21:01:12 -0500

    javascript to fetch API... still not functioning, but close

---
+++ README.md
@@ -0,0 +1,7 @@
+## Decription
+
+
+
+## Screenshot
+
+## Live URL
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,58 +1,119 @@
-var cityNameEl = document.querySelector("#city");
-var currentCityEl = document.querySelector("#current-city-form");
-var cityFormEl = document.querySelector("#city-form")
+var searchCityEl = document.querySelector("#city-input")
+var currentCityEl = document.querySelector("#current-city-form")
 
+var formSubmitHandler = function (event) {
+    //stops refreshing
+    event.preventDefault();
 
-var getWeather = function(weather) {
-    // format the weather api url **CHECK THAT THIS IS CORRECT
-    var apiUrl = "https://https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}api.openweathermap.org"; 
+    var city = document.querySelector.searchCityEl.value.trim();
 
-//make a request to URL
-fetch(apiUrl).then(function(response) {
-    if (response.ok) {
-        response.json().then(function(data){
-            displayWeather(data);
-        });
+    if (city) {
+        getCityWeather(city);
     } else {
-        alert("City Not Found!");
+        alert("please enter a city!")
     }
-})
-.catch(function(error) {
-    //notice this '.catch()' getting choianed onto the end of the .then method
-    alert("Unable to find City!")
-  });
-  response.json().then(function(data) {
-    displayWeather(data);
-  });
+};
+
+
+
+var getCityWeather = function (city) {
+    var lon;
+    var lat;
+
+    //example with London plugged in
+    var cityApi = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=974e7f7c498a1d90f5f12aeb4fe7e9e2";
+    fetch(cityApi)
+    console.log("Function is running")
+    // fetch(cityApi)
+    //     .then(function (response) {
+    //         return response.json();
+    //     }).then(function (data) 
+
+    //         //determines the city using lat and long from the search entered
+    //         lat = data.coord.lat;
+    //         lon = data.coord.lon
+
+    //         return fetch("https://api.openweathermap.org/data/2.5/weather?lat=" + lat, "&lon=" + lon, "&exclude=minutely,hourly&units=imperial&appid=974e7f7c498a1d90f5f12aeb4fe7e9e2");
+    //     }
+    //     )
 }
 
 
 
-var citySubmit = function (event) {
-    event.preventDefault ();
-//get value from city input
-var city = cityNameEl.value.trim();
-
-if (city) {
-    getWeather(city);
-    cityNameEl.value = "";
-} else {
-    alert("Please enter a city name");
-}
-}
+getCityWeather();
 
 
 
-var displayWeather = function (data, searchTerm) {
-
-    //check if apie returned any cities
-    if (city.length === 0) {
-        currentCityEl.textContent = "No Forecasts Found";
-        return;
-    }
-    console.log(city);
-}
 
 
 
-cityFormEl.addEventListener("submit", citySubmit);
+
+
+
+
+
+
+// FIRST TRY AT THE PROJECT
+// var cityNameEl = document.querySelector("#city");
+// var currentCityEl = document.querySelector("#current-city-form");
+// var cityFormEl = document.querySelector("#city-form")
+
+
+
+// //function to get city's data from API
+// var getWeather = function (city) {
+//     // EXAMPLE URL FOR LONDON
+//     var apiUrl = "api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=974e7f7c498a1d90f5f12aeb4fe7e9e2"
+//     console.log(fetch(apiUrl))
+//     console.log(response)
+
+//     //make a request to URL
+//     fetch(apiUrl)
+//         .then(function (response) {
+//             console.log("city")
+//             if (response.ok) {
+//                 response.json().then(function (data) {
+//                     displayWeather();
+//                     console.log("o")
+//                 });
+//             } else {
+//                 alert("City Not Found!");
+//             }
+//         })
+//         .catch(function (error) {
+//             //notice this '.catch()' getting choianed onto the end of the .then method
+//             alert("Unable to find City!")
+//         });
+//     response.json().then(function (data) {
+//         displayWeather(data);
+//     });
+// }
+// getWeather()
+
+
+// var citySubmit = function (event) {
+//     event.preventDefault();
+//     //get value from city input
+//     var city = cityNameEl.value.trim();
+
+//     if (city) {
+//         getWeather(city);
+//         cityNameEl.value = "";
+//     } else {
+//         alert("Please enter a city name");
+//     }
+// }
+
+// var displayWeather = function (data, searchTerm) {
+
+//     //check if apie returned any cities
+//     if (city.length === 0) {
+//         currentCityEl.textContent = "No Forecasts Found";
+//         return;
+//     }
+//     console.log(city);
+// }
+
+
+
+// cityFormEl.addEventListener("submit", getWeather); 
